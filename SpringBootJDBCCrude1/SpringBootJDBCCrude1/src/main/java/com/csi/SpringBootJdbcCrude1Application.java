6300c4f6bd971ef6d98bbd18cb9613f9605566f4
Sushilsalunkhe <ssushilmech@gmail.com>
2021-12-14 00:44:42 +0530

    Initial Commit

---
+++ SpringBootJDBCCrude1/SpringBootJDBCCrude1/src/main/java/com/csi/SpringBootJdbcCrude1Application.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcCrude1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcCrude1Application.class, args);
+	}
+
+}
